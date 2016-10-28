f53860a97b6d26ed7d444e6b72d7a1bdf41973a3
Eric Krause <krause197@gmail.com>
2016-10-28 09:21:20 -0700

    Edit question funtionality added

--- app/components/edit-question.js
+++ app/components/edit-question.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  editQuestionForm: false,
+  actions: {
+    editQuestionForm() {
+      this.set('editQuestionForm', true);
+    },
+
+    edit(question) {
+      var params = {
+        text: this.get('text'),
+        author: this.get('author'),
+        notes: this.get('notes'),
+        tags: this.get('tags'),
+      };
+      this.set('editQuestionForm', false);
+      this.sendAction('edit', post, params);
+    }
+  }
 });
