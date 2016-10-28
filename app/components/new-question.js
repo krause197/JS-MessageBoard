471f4173456e4fd6c7ee1c2270f34e76430f774b
Eric Krause <krause197@gmail.com>
2016-10-28 09:38:26 -0700

    New question functionality added

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion1() {
+      var params = {
+        text: this.get('text'),
+        author: this.get('author'),
+        notes: this.get('notes'),
+        tags: this.get('tags'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion2', params);
+    }
+  }
 });
